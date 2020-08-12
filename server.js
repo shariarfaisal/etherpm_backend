770b51b7bbdc8f09b0a6a3a84aa64ad473803414
shariarfaisal <faisaljr623@gmail.com>
2020-08-12 23:07:06 +0600

    few changes for handover

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 require('./startup/routes')(app)
 require('./startup/db')()
 
-const PORT = 1000
+const PORT = process.env.PORT || 1000
 app.listen(PORT,() => {
   console.log(`Server running on port ${PORT}`);
 })
